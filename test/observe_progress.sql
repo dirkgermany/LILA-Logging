ec4bac487e3c5290809c21fb3446d969e3fea90b
Dirk <42852469+dirkgermany@users.noreply.github.com>
2026-02-21 01:48:30 +0100

    Update observe_progress.sql

--- test/observe_progress.sql
+++ test/observe_progress.sql
@@ -1,14 +1,13 @@
-select distinct proc.id proc_id, proc.process_name process, proc.process_start, proc.proc_steps_done proc_steps_done, detail.mon_steps_done,
-        proc.proc_steps_todo * 100 / proc.proc_steps_done percent_of_work,
+select distinct proc.id proc_id, proc.process_name process, proc.process_start, proc.proc_steps_todo, proc.proc_steps_done proc_steps_done, detail.action_count,
+        proc.proc_steps_done / proc.proc_steps_todo * 100 percent_of_work,
         round((TO_NUMBER(TO_CHAR(proc.process_start, 'SSSSS.FF3'), '99999.999', 'NLS_NUMERIC_CHARACTERS = ''. ''') - 
          TO_NUMBER(TO_CHAR(proc.last_update, 'SSSSS.FF3'), '99999.999', 'NLS_NUMERIC_CHARACTERS = ''. ''')) /-60, 2) min_work
 from remote_log proc
 join (
-    select process_id, max(mon_steps_done) mon_steps_done
-    from remote_log_detail
+    select process_id, max(action_count) action_count
+    from remote_log_mon
     group by process_id
 ) detail
     on detail.process_id = proc.id
-    and detail.mon_steps_done is not null
+    and detail.action_count is not null
 order by proc.id
-;
